652993b13604119a4efd7f12b92210a387331866
Alumno <Alumno@PC21>
2024-05-22 19:40:27 -0300

    Agregar archivos de proyecto.

---
+++ frmReserva.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace pryPerezTPIntegrador
+{
+    partial class frmReserva
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbLocal = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReservar = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tblPnlAsientosAzul = new System.Windows.Forms.TableLayoutPanel();
+            this.tblPnlAsientosVerde = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbLocal
+            // 
+            this.cmbLocal.FormattingEnabled = true;
+            this.cmbLocal.Items.AddRange(new object[] {
+            "Quenaken",
+            "Onas",
+            "Tobas"});
+            this.cmbLocal.Location = new System.Drawing.Point(97, 12);
+            this.cmbLocal.Name = "cmbLocal";
+            this.cmbLocal.Size = new System.Drawing.Size(121, 21);
+            this.cmbLocal.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Local: ";
+            // 
+            // btnReservar
+            // 
+            this.btnReservar.Location = new System.Drawing.Point(370, 9);
+            this.btnReservar.Name = "btnReservar";
+            this.btnReservar.Size = new System.Drawing.Size(85, 23);
+            this.btnReservar.TabIndex = 4;
+            this.btnReservar.Text = "Reservar";
+            this.btnReservar.UseVisualStyleBackColor = true;
+            this.btnReservar.Click += new System.EventHandler(this.btnReservar_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.tblPnlAsientosAzul);
+            this.flowLayoutPanel1.Controls.Add(this.tblPnlAsientosVerde);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(26, 93);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(527, 290);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // tblPnlAsientosAzul
+            // 
+            this.tblPnlAsientosAzul.BackColor = System.Drawing.Color.LightBlue;
+            this.tblPnlAsientosAzul.ColumnCount = 5;
+            this.tblPnlAsientosAzul.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblPnlAsientosAzul.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblPnlAsientosAzul.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tblPnlAsientosAzul.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 98F));
+            this.tblPnlAsientosAzul.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 107F));
+            this.tblPnlAsientosAzul.Location = new System.Drawing.Point(3, 3);
+            this.tblPnlAsientosAzul.Name = "tblPnlAsientosAzul";
+            this.tblPnlAsientosAzul.RowCount = 4;
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblPnlAsientosAzul.Size = new System.Drawing.Size(524, 125);
+            this.tblPnlAsientosAzul.TabIndex = 0;
+            // 
+            // tblPnlAsientosVerde
+            // 
+            this.tblPnlAsientosVerde.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.tblPnlAsientosVerde.ColumnCount = 2;
+            this.tblPnlAsientosVerde.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblPnlAsientosVerde.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblPnlAsientosVerde.Location = new System.Drawing.Point(3, 134);
+            this.tblPnlAsientosVerde.Name = "tblPnlAsientosVerde";
+            this.tblPnlAsientosVerde.RowCount = 2;
+            this.tblPnlAsientosVerde.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblPnlAsientosVerde.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblPnlAsientosVerde.Size = new System.Drawing.Size(524, 114);
+            this.tblPnlAsientosVerde.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(41, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "ESCENARIO";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmReserva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 395);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnReservar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbLocal);
+            this.Name = "frmReserva";
+            this.Text = "Reserva de Asientos";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbLocal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReservar;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tblPnlAsientosAzul;
+        private System.Windows.Forms.TableLayoutPanel tblPnlAsientosVerde;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ frmReserva.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace pryPerezTPIntegrador
+{
+    public partial class frmReserva : Form
+    {
+        public frmReserva()
+        {
+            InitializeComponent();
+        }
+
+        private void tblPnlAsientosAzul_Paint(object sender, PaintEventArgs e)
+        {
+            tblPnlAsientosAzul.ColumnCount = 5;
+            //tblPnlAsientosAzul.ColumnStyles.Clear();
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    tblPnlAsientosAzul.ColumnStyles.Add(new ColumnStyle(SizeType.Percent,
+            //    (float)20));
+            //}
+
+        }
+
+        private void btnReservar_Click(object sender, System.EventArgs e)
+        {
+
+            if (cmbLocal.SelectedItem != null)
+            {
+                if (cmbLocal.SelectedItem.ToString() == "Quenaken")
+                {
+               
+                    tblPnlAsientosAzul.ColumnCount = 5;
+                    tblPnlAsientosAzul.RowCount = 4;
+
+                    tblPnlAsientosVerde.ColumnCount = 5;
+                    tblPnlAsientosVerde.RowCount = 4;
+                }
+                else if (cmbLocal.SelectedItem.ToString() == "Onas")
+                {
+                    tblPnlAsientosAzul.ColumnCount = 4;
+                    tblPnlAsientosAzul.RowCount = 10;
+
+                    tblPnlAsientosVerde.ColumnCount = 5;
+                    tblPnlAsientosVerde.RowCount = 4;
+                }
+                else if (cmbLocal.SelectedItem.ToString() == "Tobas")
+                {
+                    tblPnlAsientosAzul.ColumnCount = 3;
+                    tblPnlAsientosAzul.RowCount = 4;
+
+                    tblPnlAsientosVerde.ColumnCount = 5;
+                    tblPnlAsientosVerde.RowCount = 3;
+                }
+            }
+            tblPnlAsientosAzul.BorderStyle = BorderStyle.FixedSingle; // Puedes usar Fixed3D si prefieres un estilo tridimensional
+            tblPnlAsientosVerde.BorderStyle = BorderStyle.FixedSingle;
+        }
+    }
+}
